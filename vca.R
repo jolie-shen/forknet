0ad7ce50afebab914aa15e6e2c375bd83a06b9ce
jolie-shen <64868752+jolie-shen@users.noreply.github.com>
2020-06-27 08:56:56 -0700

    exclude zeros

--- vca.R
+++ vca.R
@@ -12,8 +12,10 @@ setwd("~/Downloads")
 normalglom <- read.csv("~/downloads/normalglom.csv", header = TRUE, 
   colClasses = c("factor", "factor", "numeric"))
 
+#take out zeros
+normalglom_nozero <- normalglom[normalglom$FPW != 0, ]
 
-varPlot(FPW~Biopsy/Glom, normalglom,
+varPlot(FPW~Biopsy/Glom, normalglom_nozero,
         BG=list(var="Biopsy", col=paste0("gray", c(100, 80, 60))),
         Points=list(pch=16, col= "red", cex=1.25),
         MeanLine=list(var=c("int", "Biopsy"), col=c("black", "orange"), lwd=c(2,2)),
@@ -23,7 +25,8 @@ varPlot(FPW~Biopsy/Glom, normalglom,
         VCnam=list(font=2, cex=1.5),
         VarLab=list(list(cex=0.5, font=2), list(cex=0.25, font=2)))
 
-sample <-normalglom[sample(nrow(normalglom), 5000), ]
+#use if data is too long
+sample <-normalglom_nozero[sample(nrow(normalglom_nozero), 5000), ]
 
 #Fit using ANOVA
 fit.anova <- fitVCA(FPW~Biopsy/Glom, normalglom)
@@ -34,8 +37,10 @@ inf <- VCAinference(fit.anova, VarVC=TRUE)
 inf
 
 #Fit using REML
+
 fit.reml <- fitVCA(FPW~Biopsy/Glom, normalglom, method = "REML")
 fit.reml
+
 
 
 ####WITH NEW LAYER IF WE GET INDIVIDUAL FOOT PROCESS DATA
@@ -54,3 +59,28 @@ varPlot(FPW~Biopsy/Glom, normalglom,
     # and how these are plotted
     VLine=list(var="Glom", col="gray75"),
     VarLab=list(list(cex=1.5), list(cex=1.25), list(cex=1.25)))
+
+
+##doing the same for Fabry
+fabryglom <- read.csv("~/downloads/fabryglom.csv", header = TRUE, 
+  colClasses = c("factor", "factor", "numeric"))
+
+fabrygom_nozero <- fabryglom[fabryglom$FPW != 0, ]
+
+varPlot(FPW~Biopsy/Glom, fabryglom_nozero,
+        BG=list(var="Biopsy", col=paste0("gray", c(100, 80, 60))),
+        Points=list(pch=16, col= "red", cex=1.25),
+        MeanLine=list(var=c("int", "Biopsy"), col=c("black", "orange"), lwd=c(2,2)),
+        Mean=list(col="cyan", cex=1.25, lwd=2), las=1,
+        YLabel=list(text="FPW", las=0, line=3, font=2, cex=1.25),
+        Title=list(main="Variance Component Analysis for Fabry", cex.main=1.75),
+        VCnam=list(font=2, cex=1.5),
+        VarLab=list(list(cex=0.5, font=2), list(cex=0.25, font=2)))
+
+#Fit using ANOVA
+fit.anova.fabry <- fitVCA(FPW~Biopsy/Glom, fabryglom_nozero)
+fit.anova
+
+#estimate 95% confidence intervals, request CI for all variance components via 'VarVC=TRUE'
+inf.fabry <- VCAinference(fit.anova.fabry, VarVC=TRUE)
+inf.fabry
